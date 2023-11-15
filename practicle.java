a2cbb05b4c25057e8f295cea35f7fd31102fb46c
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-11-15 08:04:04 +0530

    day01

---
+++ practicle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class  practicle{
+public static void main(String[] args) {
+
+    /* 
+    int x=5;
+    System.out.println(x++);
+    System.out.println(++x);*/
+
+
+    /* 
+    constant int y = 10;
+    final int c = 10;
+    static const int d = 10;
+    const int z = 10;*/
+
+    /* 
+    String str1= "hello";
+    String str2 = new String("hello");
+    System.out.println(str1==str2);
+    */
+
+   Scanner myObj = new Scanner(System.in);
+   System.out.println("Enter Number 01:"); 
+   String UserName = myObj.nextLine();
+
+   System.out.pr
+
+
+}}
